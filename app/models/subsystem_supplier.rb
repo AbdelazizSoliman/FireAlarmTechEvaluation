4d926de665112fca49667a25c9a8ac2045f70541
Abdelaziz Soliman <abdelaziz.soliman89@gmail.com>
2025-01-11 08:06:04 +0200

    Remove default notifuication

--- app/models/subsystem_supplier.rb
+++ app/models/subsystem_supplier.rb
@@ -6,13 +6,4 @@ class SubsystemSupplier < ApplicationRecord
 
   private
 
-  def create_notification
-    Notification.create!(
-      title: "New Project Assigned",
-      body: "Great news, you were assigned a new project: #{subsystem.name}. Please check your projects.",
-      notifiable: self.supplier,
-      read: false,
-      status: "unread"
-    )
-  end
 end

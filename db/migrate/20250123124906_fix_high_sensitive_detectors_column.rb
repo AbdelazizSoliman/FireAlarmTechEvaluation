ffd803113458586c78ee0b0a4c36228ab600db38
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-02-06 20:07:35 +0200

    Conditionally rename high_sensitive_detectors column if it exists

--- db/migrate/20250123124906_fix_high_sensitive_detectors_column.rb
+++ db/migrate/20250123124906_fix_high_sensitive_detectors_column.rb
@@ -1,5 +1,9 @@
 class FixHighSensitiveDetectorsColumn < ActiveRecord::Migration[7.1]
   def change
-    rename_column :detectors_field_devices, :high_sensitive_detectors_for_harsh, :high_sensitive_detectors_for_harsh_environments
+    if column_exists?(:detectors_field_devices, :high_sensitive_detectors_for_harsh)
+      rename_column :detectors_field_devices, :high_sensitive_detectors_for_harsh, :high_sensitive_detectors_for_harsh_environments
+    else
+      say "Column high_sensitive_detectors_for_harsh does not exist on detectors_field_devices. Skipping rename."
+    end
   end
 end

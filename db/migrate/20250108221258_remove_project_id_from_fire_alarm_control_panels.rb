f08933b788b522466ddd2bf06824040ba550822c
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-02-12 14:33:31 +0200

    Fix migration by checking if project_id column exists

--- db/migrate/20250108221258_remove_project_id_from_fire_alarm_control_panels.rb
+++ db/migrate/20250108221258_remove_project_id_from_fire_alarm_control_panels.rb
@@ -1,5 +1,6 @@
 class RemoveProjectIdFromFireAlarmControlPanels < ActiveRecord::Migration[7.1]
   def change
-    remove_reference :fire_alarm_control_panels, :project, null: false, foreign_key: true
-  end
+    if column_exists?(:fire_alarm_control_panels, :project_id)
+      remove_reference :fire_alarm_control_panels, :project, null: false, foreign_key: true
+    end
 end

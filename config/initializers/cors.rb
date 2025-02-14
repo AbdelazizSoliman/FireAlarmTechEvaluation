0a03142cc94293240f10aa545b67a16e8fe1ab3a
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-02-14 17:27:02 +0200

    Fix CORS & API URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:5173', 'https://supplier-portal-bz2n8nahf-abdelazizsolimans-projects.vercel.app/'
+      origins 'http://localhost:5173', 'https://supplier-portal-bz2n8nahf-abdelazizsolimans-projects.vercel.app'
       resource '*', headers: :any, expose: ['Authorization'], methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
     end

e06b3760bbeecd2b7a0a25e8c8209612a7ee5f5d
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-04-02 21:32:48 +0200

    Add condition for migration in deployment

--- db/migrate/20250402154359_remove_staff_consoles_module_type_from_nurse_station_terminal_module.rb
+++ db/migrate/20250402154359_remove_staff_consoles_module_type_from_nurse_station_terminal_module.rb
@@ -1,5 +1,9 @@
 class RemoveStaffConsolesModuleTypeFromNurseStationTerminalModule < ActiveRecord::Migration[7.1]
   def change
-    remove_column :nurse_station_terminal_module, " Staff Consoles Module Type", :string
+    if column_exists?(:nurse_station_terminal_module, " Staff Consoles Module Type")
+      remove_column :nurse_station_terminal_module, " Staff Consoles Module Type", :string
+    elsif column_exists?(:nurse_station_terminal_module, :staff_consoles_module_type)
+      remove_column :nurse_station_terminal_module, :staff_consoles_module_type, :string
+    end
   end
 end

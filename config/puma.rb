025207b51fef0afd69ed630b22519f11ff455a0c
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-05-19 23:44:44 +0300

    Update puma for development mode

--- config/puma.rb
+++ config/puma.rb
@@ -5,29 +5,24 @@ max_threads = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
 min_threads = Integer(ENV.fetch("RAILS_MIN_THREADS", max_threads))
 threads min_threads, max_threads
 
-# How many workers (processes) to spin up
-# In production use ENV['WEB_CONCURRENCY'], or default to 2
-workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
-
-# Preload the app before forking workers for lower memory & faster boots
-preload_app!
+# Only spin up clustered workers on non-Windows platforms
+unless Gem.win_platform?
+  workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+  preload_app!
+end
 
 # Always bind to the port provided by the environment
-port ENV.fetch("PORT", 3000)
+port        ENV.fetch("PORT",        3000)
 
 # Rack environment
 environment ENV.fetch("RAILS_ENV", "development")
 
-# Don't block on worker booting after fork
+# If you do end up forking (i.e. on non-Windows), reconnect ActiveRecord here
 on_worker_boot do
-  # Reconnect any threadsafe clients here, e.g. ActiveRecord
-  if defined?(ActiveRecord)
-    ActiveRecord::Base.establish_connection
-  end
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
 # Extend the default worker timeout (in seconds) to 1 hour
-# Pick up from ENV or fallback
 worker_timeout Integer(ENV.fetch("PUMA_WORKER_TIMEOUT", 3600))
 
 # Allow puma to be restarted by `rails restart` command
